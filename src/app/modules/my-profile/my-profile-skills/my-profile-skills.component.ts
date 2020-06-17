ec02cb82015047d9d0ae270b01ad69a56a5be497
rizkymf <rizkymf>
2020-06-17 10:50:12 +0700

    first commit

--- src/app/modules/my-profile/my-profile-skills/my-profile-skills.component.ts
+++ src/app/modules/my-profile/my-profile-skills/my-profile-skills.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { onConstructTableHeader } from 'app/shared/utils';
 import { TableColumn } from 'app/shared/models/table.interface';
 
+export interface skill{
+  skill,
+  level
+}
+
 @Component({
   selector: 'app-my-profile-skills',
   templateUrl: './my-profile-skills.component.html',
@@ -9,11 +14,15 @@ import { TableColumn } from 'app/shared/models/table.interface';
 })
 export class MyProfileSkillsComponent implements OnInit {
 
-  skillColumns: TableColumn[]
+  skillColumns: TableColumn[];
+  skills: skill[] = [];
+  skill: skill;
+  selectedSkill: skill;
+  displayDialog: boolean;
+  newSkill;
 
   constructor() { 
     this.skillColumns = onConstructTableHeader([
-      '',
       'Skill',
       'Level',
     ]);
@@ -22,4 +31,48 @@ export class MyProfileSkillsComponent implements OnInit {
   ngOnInit() {
   }
 
+  cloneSkill(s: skill): skill {
+    let skill : skill = {
+      level: '',
+      skill: '',
+    };
+    for(let prop in s){
+      skill[prop]=s[prop]
+    }
+    return skill;
+  }
+
+  addSkill(){
+    this.newSkill = true;
+    this.skill = {
+      level: '',
+      skill: '',
+    };
+    this.displayDialog = true;
+  }
+
+  onSave() {
+    let skills = [...this.skills];
+    if(this.newSkill){
+      skills.push(this.skill);
+    } else {
+      skills[this.skills.indexOf(this.selectedSkill)]=this.skill;
+    }
+    this.skills = skills;
+    this.skill = null;
+    this.displayDialog = false;
+  }
+
+  onDelete() {
+    let index = this.skills.indexOf(this.selectedSkill);
+    this.skills = this.skills.filter((val, i) => i != index);
+    this.skill = null;
+    this.displayDialog = false;
+  }
+
+  onRowSelect(event){
+    this.newSkill = false;
+    this.skill = this.cloneSkill(event.data);
+    this.displayDialog = true;
+  }
 }

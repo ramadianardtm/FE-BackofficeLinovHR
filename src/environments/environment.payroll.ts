148b834069f5b30ccf845d80c86a0bb3811a2cb3
Marulam Heryanto Samosir <marulam_samosir@yahoo.com>
2020-06-17 10:38:48 +0700

    remove unused env

--- src/environments/environment.payroll.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://api.linovhr.com/payroll/api/v1/',
-    mockApiUrl: 'https://localhost:3000/',
-    reportUrl: 'https://api.linovhr.com:7080/report/',
-    socketUrl: 'https://api.linovhr.com/payroll/api/v1/socket',
-    translateUrl: '/assets/i18n/',
-};

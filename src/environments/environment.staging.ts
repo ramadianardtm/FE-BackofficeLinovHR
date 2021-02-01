fc6a6545e07c1694e1761643731654f7ce8e87ca
sugengekosaputro <sugeng.eko@lawencon.com>
2021-02-01 15:23:40 +0700

    change env

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://api-dev4.linovhr.com:8080/linov-back/api/v1/',
+    apiUrl: 'http://api-dev4.linovhr.com:8080/linov-bo/api/v1/',
     mockApiUrl: 'http://localhost:3000/',
     reportUrl: 'http://api-dev.linovhr.com:7080/report/',
     translateUrl: '/assets/i18n/',
